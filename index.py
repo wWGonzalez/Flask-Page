802f46518d5b819011069acf51b5548f815db46b
wer <werner.e.2.g@gmail.com>
2020-01-05 09:16:20 -0600

    Pagina creada en Python con Flas

---
+++ index.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    #num1 = 5
+    #num2 = 4
+    #suma = num1 + num2
+    #return 'Suma :'+str(suma)
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
